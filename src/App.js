616cf9692f5023c724c1ed548377e277d9338637
hdrwilkinson <harry@harrywilkinson.com>
2021-11-26 16:24:42 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -5,8 +5,19 @@ function App() {
       <div className="twinkling"></div> 
       <div className="clouds"></div>
       <div className="titleWrapper">
+        <div className="logo">
+          <img src="/PSY Logo.png" alt="PSY Network Logo"/>
+        </div>
         <div className="title">
-          <h1><span>PSY</span><br/>Network</h1>
+          <h2>Prepare for the PSY Network Launch</h2>
+        </div>
+        <div className="social">
+          <div className="socialLink">
+            <a target="_blank" href="https://twitter.com/Shroomz_NFT"><i class="fab fa-twitter"></i></a>
+          </div>
+          <div className="socialLink">
+            <a target="_blank" href="http://discord.gg/SbKRhMHF88"><i class="fab fa-discord"></i></a>
+          </div>
         </div>
       </div>
     </div>
