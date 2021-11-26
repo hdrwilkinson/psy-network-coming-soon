e748153d37d9eb35bb851865c7230d8eee5b96f9
hdrwilkinson <harry@harrywilkinson.com>
2021-11-26 15:12:50 +0000

    Upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="stars"></div>
+      <div className="twinkling"></div> 
+      <div className="clouds"></div>
+      <div className="titleWrapper">
+        <div className="title">
+          <h1><span>PSY</span><br/>Network</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
